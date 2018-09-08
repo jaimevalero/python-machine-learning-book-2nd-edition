11177d18d57df1631992508a62bdd7a843f542a8
jaimevalero <jaimevalero78@yahoo.es>
2018-09-08 15:41:48 +0200

    Computer vision exercise

--- ejercicios/tpot_iris_pipeline.py
+++ ejercicios/tpot_iris_pipeline.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import Normalizer
+from sklearn.neighbors import KNeighborsClassifier
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,11 +9,8 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.9818181818181818
-exported_pipeline = make_pipeline(
-    Normalizer(norm="l1"),
-    GaussianNB()
-)
+# Score on the training set was:0.9826086956521738
+exported_pipeline = KNeighborsClassifier(n_neighbors=6, p=2, weights="distance")
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
